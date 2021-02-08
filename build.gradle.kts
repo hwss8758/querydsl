78a526e8ebd58301d0227de1f97d541fb712ee4d
wonsang.hwang <wonsang.hwang@navercorp.com>
2021-02-08 15:43:14 +0900

    querydsl build.gradle.kts 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,13 @@ plugins {
      * jpa 양방향 무한로프 방지
      */
     id("org.jetbrains.kotlin.plugin.allopen") version "1.3.61"
+
+    /**
+     * query DSL
+     */
+    id("com.ewerk.gradle.plugins.querydsl") version "1.0.10"
+    kotlin("kapt") version "1.3.61"
+    idea
 }
 
 group = "study"
@@ -19,6 +26,7 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
+    maven("https://plugins.gradle.org/m2/")
 }
 
 dependencies {
@@ -29,6 +37,19 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    /**
+     * querydsl start
+     */
+    // 4.4.0 => 최신버전으로 설정해 줘야 에러가 나지 않는다.
+    api("com.querydsl:querydsl-jpa:4.4.0")
+    // kapt로 dependency를 지정해 준다.
+    // kotlin 코드가 아니라면 kapt 대신 annotationProcessor를 사용한다.
+    kapt("com.querydsl:querydsl-apt:4.4.0:jpa") // ":jpa 꼭 붙여줘야 한다!!"
+    //kapt("org.hibernate.javax.persistence:hibernate-jpa-2.1-api:1.0.2.Final")
+    /**
+     * querydsl end
+     */
 }
 
 tasks.withType<KotlinCompile> {
@@ -50,3 +71,18 @@ allOpen {
     annotation("javax.persistence.MappedSuperclass")
     annotation("javax.persistence.Embeddable")
 }
+
+/**
+ * querydsl start
+ */
+// 생성된 QClass들을 intelliJ IDEA가 사용할 수 있도록 소스코드 경로에 추가해 준다.
+idea {
+    module {
+        val kaptMain = file("$buildDir/generated/source/kapt/main")
+        sourceDirs.add(kaptMain)
+        generatedSourceDirs.add(kaptMain)
+    }
+}
+/**
+ * querydsl end
+ */
